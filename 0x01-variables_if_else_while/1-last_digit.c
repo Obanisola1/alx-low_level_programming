ca748b6edabcbcb89afaa5cb3f78d13c7fa9a4c8
Obanisola1 <abioyesaheed@gmail.com>
2022-10-14 05:15:40 -0700

    String task commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Last_digit
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 6 && n != 0)
+	{
+		printf("%d and is less than 6 and not 0\n", n);
+	} else if (n == 0)
+	{
+		printf("%d and is 0\n", n);
+	} else
+	{
+		printf("%d and is greater than 5\n", n);
+	}
+	return (0);
+}
