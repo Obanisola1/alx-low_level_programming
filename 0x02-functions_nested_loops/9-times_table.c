c69e29026c50d3f8603634b8bd530bd5272d076f
Obanisola1 <abioyesaheed@gmail.com>
2022-10-18 14:31:36 -0700

    9-task commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#inlcude "main.h"
+#include "main.h"
 
 /**
  * times_table - print the 9 times table
@@ -9,7 +9,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(' ');
+		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
 		{
