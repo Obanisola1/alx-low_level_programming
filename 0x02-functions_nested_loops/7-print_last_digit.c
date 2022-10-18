19dff60ef438dbfb047a00036f489b4f32be36df
Obanisola1 <abioyesaheed@gmail.com>
2022-10-18 14:09:52 -0700

    task commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	int ld = n % 10;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
 
-	_putchar(x + '0');
-
-	return (0);
+	return (ld);
 }
