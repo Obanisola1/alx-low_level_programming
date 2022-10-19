186ebc9d7cdee0ed7e8a53633599873fadf1f8d5
Obanisola1 <abioyesaheed@gmail.com>
2022-10-19 08:17:09 -0700

    task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,7 +27,7 @@ int main(void)
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
-	fib2_halfs = fib2 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 
 	for (count = 93; count < 99; count++)
 	{
@@ -46,6 +46,6 @@ int main(void)
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-	print("\n");
+	printf("\n");
 	return (0);
 }
